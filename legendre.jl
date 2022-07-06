4a59c09faa103f7408c89e519a3e379ace6e5ba1
William Burgess <williamburgess02@gmail.com>
2022-07-06 20:39:53 +0100

    Update legendre.jl

--- legendre.jl
+++ legendre.jl
@@ -1,11 +1,16 @@
 using ClassicalOrthogonalPolynomials, Plots, SpecialFunctions
 
 # compute fourier transform of m degree Legendre poly
-function LegFT(m) 
+function legendreft(m) 
     f = x -> 2 * (-1im)^m * sphericalbesselj(m, x)
     return f
 end
 
 # compute legendre series of a poly
-
+function legendreseries(f)
+    T = Legendre()
+    x = axes(T, 1)
+    c = T \ f.(x)
+    return c
+end
 # compute fourier transform of poly using leg series
