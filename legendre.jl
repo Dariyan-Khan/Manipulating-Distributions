63c2e6ab8f6bb43e79708080f7c7ad1ad97862d9
William Burgess <williamburgess02@gmail.com>
2022-07-07 10:04:17 +0100

    Update legendre.jl

--- legendre.jl
+++ legendre.jl
@@ -21,3 +21,32 @@ function legendreseries(f)
 end
 
 # compute fourier transform of poly using leg series
+function bleft(k, n, f, g)
+    α = legendrecoeff(f)
+    β = legendrecoeff(g)
+    if k > n
+        if n == 0
+            if k == 0
+                return α[1] - α[2]/3
+            else
+                return α[k-1]/(2k-1) - α[k+1]/(2k+3)
+            end
+        if n == 1
+            if k == 0
+                return -bleft(1, 0, f, g)/3
+            else
+                return bleft(k-1, 0, f, g)/(2k-1) - bleft(k,0,f,g) - bleft(k+1,0,f,g)/(2k+3)
+            end
+        else
+            return -(2n-1)/(2k+3) * bleft(k+1,n-1,f,g) + (2n-1)/(2k-1) * bleft(k-1,n-1,f,g) + bleft(k, n-2,f,g)
+        end
+    else
+        return bleft(n,k,f,g) * (-1)^(n+k) * (2k+1)/(2n+1)
+    end
+end
+end
+
+function gammaleft(k, f, g)
+    # find degree of f 
+    # take sum and use bleft 
+end
